db5676b44d493023857a0a3bc31fed51e5c6ba67
Francesco518 <fbuonocore655@gmail.com>
2024-03-04 21:21:53 +0100

    Initial commit

---
+++ src/test/java/francescobuonocore/capstone/CapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package francescobuonocore.capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
